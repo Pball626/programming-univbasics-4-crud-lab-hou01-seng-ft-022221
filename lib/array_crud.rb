8cff6aca1608adad443f8ff6830afaa296af6f78
Jose Mendoza <pball626@gmail.com>
2021-02-06 19:35:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,6 +20,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = ["gold", "silver"]
+  remove_element_from_end_of_array.pop ()
   
 end
 
