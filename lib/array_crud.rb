33060ab2993a6ee536405d3f17dc46d7e7d4fba0
Jose Mendoza <pball626@gmail.com>
2021-02-06 19:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -50,5 +50,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
+  update_element_from_index = ["wow", "red", "blue", "green"]
+  update_element_from_index[3 = "orange"]
 
 end
